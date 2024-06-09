396817d24ccaf4b544a6fccfef32645b5981af31
Massimo <s319898@studenti.polito.it>
2024-06-09 18:32:32 +0200

    fixed sva_if

---
+++ tb/rca_if_sva.svh
@@ -0,0 +1,31 @@
+`ifndef RCA_IF_SVA
+`define RCA_IF_SVA
+
+
+int unsigned err_num=0;
+
+function int unsigned get_err_num();
+    automatic int unsigned n = err_num;
+    err_num = 0;
+    return n;
+endfunction: get_err_num
+
+
+property SUM_result;
+   
+     @(negedge clk) 
+    
+        // Calculate the expected sum and carry out
+        SUM == A + B + Cin;
+       // Cout == (A + B + Cin) >= (1 << N);        
+		
+endproperty
+
+// Assertion instantiation
+/* assert property(SUM_result);
+    else begin
+	err_num++;     
+	$error("Ripple Carry Adder check failed.");
+    end
+*/	
+`endif // RCA_IF_SVA
