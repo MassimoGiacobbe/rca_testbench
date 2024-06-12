3b5003a40db6ec9645ede12b923058a076db2bca
Massimo <s319898@studenti.polito.it>
2024-06-12 16:49:22 +0200

    fixed sva_if

--- sim/compile_sv.f
+++ sim/compile_sv.f
@@ -1,8 +1,4 @@
 ../tb/rca_if.sv
-../tb/rca_if_sva.svh
 ../tb/rca_wrap.sv
-../tb/rca_cov.sv
-../tb/rca_tester.sv
-../tb/rca_verbose_tester.sv
 ../tb/rca_tb.sv
 
--- tb/rca_if_sva.svh
+++ tb/rca_if_sva.svh
@@ -1,29 +1,31 @@
-`ifndef RCA_IF_SVA
-`define RCA_IF_SVA
+`ifndef RCA_IF_SVA_H_
+`define RCA_IF_SVA_H_
 
-
-int unsigned err_num=0;
+int unsigned err_num = 0;
 
 function int unsigned get_err_num();
-    automatic int unsigned n = err_num;
-    err_num = 0;
-    return n;
-endfunction: get_err_num
+	automatic int unsigned val =err_num;	
+	err_num=0;
+	return val;
+endfunction : get_err_num
 
-
-property SUM_result;
-    	@(negedge clk)
-        // Calculate the expected sum and carry out
-        SUM == A + B + Cin;
-       // Cout == (A + B + Cin) >= (1 << N);        
-		
+property p_sum;
+	@(posedge clk)
+		(A+B > ((1<<N)-1)) -> SUM == ((A+B)-(1<<N));
 endproperty
 
-// Assertion instantiation
-a_result : assert property(SUM_result);
-    else begin
-		err_num++;     
-		$error("Ripple Carry Adder check failed.");
-    end
-	
-`endif // RCA_IF_SVA
+
+property p_cout;
+	@(posedge clk)
+		(A+B > (1<<N)-1) -> Cout == 1;
+endproperty
+
+
+assert property(p_sum and p_cout)
+else
+begin
+	$display("Error: ...");
+	err_num ++;
+end
+
+`endif
